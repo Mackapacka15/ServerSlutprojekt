9a2cf6e6a3e670bcf04a636db2865b3f56c5ffbe
Markus <Markuspiotrowski@live.com>
2022-03-15 13:16:37 +0100

    Id-PutRequest

--- ServerSlutprojekt/Controllers/AttendanceController.cs
+++ ServerSlutprojekt/Controllers/AttendanceController.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
@@ -10,17 +11,19 @@ namespace ServerSlutprojekt.Controllers
     [ApiController]
     public class AttendanceController : ControllerBase
     {
+        static Random generator = new Random();
 
         static AttendanceController()
         {
             string peopleString = System.IO.File.ReadAllText("People.json");
-            Person.People = JsonSerializer.Deserialize<List<Person>>(peopleString);
+            Person.people = JsonSerializer.Deserialize<List<Person>>(peopleString);
         }
 
         [HttpGet]
         public ActionResult Get(string name)
         {
-            bool personExists = Exists(name);
+            string[] split = name.Split('─');
+            bool personExists = Exists(split[0], split[1]);
 
             if (personExists)
             {
@@ -35,12 +38,12 @@ namespace ServerSlutprojekt.Controllers
         [HttpPut]
         public ActionResult Put(string name)
         {
-
-
-            if (!Exists(name))
+            string[] split = name.Split('─');
+            if (!Exists(split[0], split[1]))
             {
-                string[] split = name.Split('─');
-                new Person(split[0], split[1]);
+                new Person(split[0], split[1], GenerateId());
+                SaveData();
+                LoadData();
                 return Ok();
             }
             else
@@ -49,17 +52,47 @@ namespace ServerSlutprojekt.Controllers
             }
         }
 
-        private bool Exists(string name)
+        private bool Exists(string firstname, string lastname)
         {
-            foreach (Person item in Person.People)
+            foreach (Person item in Person.people)
             {
-                if ((item.Firstname + item.Lastname).ToLower() == name.ToLower())
+                if (item.Firstname.ToLower() == firstname.ToLower() && item.Lastname.ToLower() == lastname.ToLower())
                 {
                     item.Here = true;
                     return true;
                 }
             }
             return false;
+        }
+        static int GenerateId()
+        {
+            bool conflict = true;
+            int id = 0;
+            while (conflict)
+            {
+                conflict = false;
+                id = generator.Next(100000, 999999);
+                foreach (var item in Person.people)
+                {
+                    if (item.Id == id)
+                    {
+                        conflict = true;
+                        break;
+                    }
+                }
+            }
+            return id;
+        }
+        private void SaveData()
+        {
+            string jsonstring = JsonSerializer.Serialize<List<Person>>(Person.people);
+            System.IO.File.WriteAllText("People.json", jsonstring);
+        }
+        private void LoadData()
+        {
+            string peopleString = System.IO.File.ReadAllText("People.json");
+            Person.people.Clear();
+            Person.people = JsonSerializer.Deserialize<List<Person>>(peopleString);
         }
     }
 }
--- ServerSlutprojekt/Person.cs
+++ ServerSlutprojekt/Person.cs
@@ -5,38 +5,20 @@ namespace ServerSlutprojekt
     public class Person
     {
 
-        static public List<Person> People = new List<Person>();
-        static Random generator = new Random();
+        static public List<Person> people = new List<Person>();
         public string Firstname { get; set; }
         public string Lastname { get; set; }
 
         public bool Here { get; set; }
         public int Id { get; set; }
 
-        public Person(string firstname, string lastname)
+        public Person(string firstname, string lastname, int id)
         {
+            Id = id;
             Firstname = firstname;
             Lastname = lastname;
-            People.Add(this);
+            people.Add(this);
         }
-        static int GenerateId()
-        {
-            bool conflict = true;
-            int id = 0;
-            while (conflict)
-            {
-                conflict = false;
-                id = generator.Next(100000, 999999);
-                foreach (var item in People)
-                {
-                    if (item.Id == id)
-                    {
-                        conflict = true;
-                        break;
-                    }
-                }
-            }
-            return id;
-        }
+
     }
 }
